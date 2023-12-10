724459ed137aa751de6e758b97b8116bbfb2b708
heiyou1234 <maji.dousuru46@gmail.com>
2023-12-10 21:51:43 +0900

    修正箇所

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   resources :users, only: [:edit, :update]
+  delete '/logout', to: 'sessions#destroy'
+  # get '/users/sign_out', to: 'sessions#destroy', as: 'sign_out'
   
   # Defines the root path route ("/")
   # root "articles#index"
