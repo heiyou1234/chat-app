cb5cb7ce19507b8f99e2f51d4e2b92b21437351e
heiyou1234 <maji.dousuru46@gmail.com>
2023-12-10 20:01:10 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
+  resources :users, only: [:edit, :update]
   
   # Defines the root path route ("/")
   # root "articles#index"
